5f77a4804502ba9c31263a8ba075398f00079ce3
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-07-11 18:39:51 +0100

    console

--- routes/movieByID_route.js
+++ routes/movieByID_route.js
@@ -1,30 +1,31 @@
-const router = require('express').Router();
-const {getMovieById} = require('../controllers/movies_controller');
-const Comment = require('../controllers/comment_controller');
-const { formatTime } = require('../controllers/calcul');
-const mongoose = require('mongoose');
+const router = require("express").Router();
+const { getMovieById } = require("../controllers/movies_controller");
+const Comment = require("../controllers/comment_controller");
+const { formatTime } = require("../controllers/calcul");
+const mongoose = require("mongoose");
 
 router
-  .route('/id/:id')
+  .route("/id/:id")
   .get(async (req, res, next) => {
     try {
       const id = mongoose.Types.ObjectId(req.params.id);
       req.session.movie_id = id;
       const movie = await getMovieById(id);
       if (!movie) {
-        return res.render('movieByID', {
-          movie: { title: 'No movie with such id found in database ' },
-          title: 'Not found'
+        return res.render("movieByID", {
+          movie: { title: "No movie with such id found in database " },
+          title: "Not found"
         });
       }
       movie.runtime = formatTime(movie.runtime);
       const comments = await Comment.getMovieComments(id);
-      // console.log('comments', comments)
+      console.log("movieById =", movie);
+      console.log("comments =", comments);
       const queryYoutube = `${movie.title} ${movie.type} official trailer 
     ${movie.year} ${(movie.cast && movie.cast[0]) ||
         (movie.directors && movie.directors[0])} `;
       req.session.queryYoutube = queryYoutube;
-      res.render('movieByID', {
+      res.render("movieByID", {
         movie,
         comments,
         title: movie.title
@@ -35,7 +36,7 @@ router
   })
   .post(async (req, res) => {
     if (!req.isAuthenticated()) {
-      return res.redirect('/login');
+      return res.redirect("/login");
     }
     try {
       await Comment.addComment(
@@ -50,4 +51,4 @@ router
     }
   });
 
-  module.exports = router;
+module.exports = router;
